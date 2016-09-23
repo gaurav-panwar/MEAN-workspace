965d61c8d106d6cef7896e81bb47fda092fb093d
gaurav-panwar <panwar.gaurav93@gmail.com>
2016-09-23 12:45:20 +0530

    Created HTTP Server using Node Js

---
+++ csvtojson/myHttpServer.js
@@ -0,0 +1,32 @@
+const http = require('http');
+var fs = require('fs');
+var url = require('url');
+const PORT = 8082;
+
+//Create Server
+http.createServer(function(req, res) {
+	var pathname = url.parse(req.url).pathname;
+	
+	if(pathname == "/")
+		pathname = "/index.html";
+
+	console.log(" Request for '" + pathname + "' received." );
+
+	fs.readFile(pathname.substr(1), function(err, data) {
+		if(err) {
+			console.log(err);
+			
+			res.writeHead(404, {'Content-Type': 'text/html'});//HTTP Status: 404: NOT FOUND
+		}
+		else {
+			res.writeHead(200, {'Content-Type': 'text/html'});//Page Found. HTTP Status: 200 : OK
+
+			res.write(data);
+		}
+		res.end();
+	});
+
+
+}).listen(PORT);
+
+console.log('Server Running at http://127.0.0.1:' + PORT + "/");
--- httpServer/myHttpServer.js
+++ httpServer/myHttpServer.js
@@ -4,8 +4,31 @@ var url = require('url');
 
 //Create Server
 http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('Hello World');
-}).listen(8085);
+	// var pathname = url.parse(req.url).pathname;
+	// console.log(pathname);
+	if(req.url == "/"){
+		// pathname = "/index.html";
+		console.log(req.url);
+	}
+	// console.log(" Request for '" + pathname + "' received." );
 
-console.log('Server Running at http://127.0.0.1:8085/');
+	fs.readFile('index.html', function(err, data) {
+		if(err) {
+			console.log(err);
+			
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			
+		}
+		else {
+			//Page Found. HTTP Status: 200 : OK
+			res.writeHead(200, {'Content-Type': 'text/html'});
+
+			res.write(data);
+		}
+		res.end();
+	});
+
+
+}).listen(8082);
+
+console.log('Server Running at http://127.0.0.1:8082/');
